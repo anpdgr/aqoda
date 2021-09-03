382d9e455066223de938dcc3f06673db8b12a04f
anpdgr <ant-dgr@hotmail.com>
2021-09-03 14:44:11 +0700

    dependency

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const services = require("./services.js");
+const createService = require("./services.js");
 const {
   HotelIsFullError,
   RoomIsAlreadyBookedError,
@@ -11,7 +11,8 @@ const {
 const { Guest } = require("./model");
 const postgresClient = require("./postgres-client");
 const prismaClient = require("./prisma-client");
-
+const prismaRepositories = require("./prisma-repositories");
+const firestoreRepositories = require("./firestore-repositories");
 
 class Command {
   constructor(name, params) {
@@ -23,65 +24,66 @@ class Command {
 async function main() {
   const fileName = "input.txt";
   const commands = getCommandsFromFileName(fileName);
-  await postgresClient.query("DELETE FROM keycards; DELETE FROM rooms;")
+  await postgresClient.query("DELETE FROM keycards; DELETE FROM rooms;");
+  const application = createApplication();
   await commands.reduce(async (initial, command) => {
     await initial;
     switch (command.name) {
       case "create_hotel": {
-        await createHotel(command);
+        await application.createHotel(command);
 
         break;
       }
 
       case "book": {
-        await book(command);
+        await application.book(command);
 
         break;
       }
 
       case "book_by_floor": {
-        await bookByFloor(command);
+        await application.bookByFloor(command);
 
         break;
       }
 
       case "checkout": {
-        await checkout(command);
+        await application.checkout(command);
 
         break;
       }
 
       case "checkout_guest_by_floor": {
-        await checkoutGuestByFloor(command);
+        await application.checkoutGuestByFloor(command);
 
         break;
       }
 
       case "list_available_rooms": {
-        await listAvailableRooms(command);
+        await application.listAvailableRooms(command);
 
         break;
       }
 
       case "list_guest": {
-        await listGuests(command);
+        await application.listGuests(command);
 
         break;
       }
 
       case "list_guest_by_age": {
-        await listGuestsByAge(command);
+        await application.listGuestsByAge(command);
 
         break;
       }
 
       case "list_guest_by_floor": {
-        await listGuestsByFloor(command);
+        await application.listGuestsByFloor(command);
         break;
       }
 
       case "get_guest_in_room": {
-        await getGuestsInRoom(command);
+        await application.getGuestsInRoom(command);
         break;
       }
 
@@ -114,160 +116,176 @@ function getCommandsFromFileName(fileName) {
     );
 }
 
-async function createHotel(command) {
-  const [floor, roomPerFloor] = command.params;
+function createApplication() {
+  const services = createService(firestoreRepositories);
+  async function createHotel(command) {
+    const [floor, roomPerFloor] = command.params;
 
-  const hotel = await services.createHotel(floor, roomPerFloor);
-
-  console.log(
-    `Hotel created with ${hotel.floor} floor(s), ${hotel.roomPerFloor} room(s) per floor.`
-  );
-}
-
-async function book(command) {
-  const [roomNumber, name, age] = command.params;
-
-  //book 203 Thor 32
-  try {
-    const guest = new Guest(name, age);
-    const keycardNumber = await services.book(roomNumber.toString(), guest);
+    const hotel = await services.createHotel(floor, roomPerFloor);
 
     console.log(
-      `Room ${roomNumber} is booked by ${name} with keycard number ${keycardNumber}.`
+      `Hotel created with ${hotel.floor} floor(s), ${hotel.roomPerFloor} room(s) per floor.`
     );
-  } catch (err) {
-    switch (true) {
-      case err instanceof HotelIsFullError: {
-        console.log("Hotel is fully booked.");
-        break;
+  }
+
+  async function book(command) {
+    const [roomNumber, name, age] = command.params;
+
+    //book 203 Thor 32
+    try {
+      const guest = new Guest(name, age);
+      const keycardNumber = await services.book(roomNumber.toString(), guest);
+
+      console.log(
+        `Room ${roomNumber} is booked by ${name} with keycard number ${keycardNumber}.`
+      );
+    } catch (err) {
+      switch (true) {
+        case err instanceof HotelIsFullError: {
+          console.log("Hotel is fully booked.");
+          break;
+        }
+        case err instanceof RoomIsAlreadyBookedError: {
+          console.log(
+            `Cannot book room ${err.room.roomNumber} for ${name}, The room is currently booked by ${err.room.guest.name}.`
+          );
+          break;
+        }
+        default:
+          throw err;
       }
-      case err instanceof RoomIsAlreadyBookedError: {
-        console.log(
-          `Cannot book room ${err.room.roomNumber} for ${name}, The room is currently booked by ${err.room.guest.name}.`
-        );
-        break;
-      }
-      default:
-        throw err;
     }
   }
-}
 
-async function bookByFloor(command) {
-  //book_by_floor 1 TonyStark 48
-  const [floor, name, age] = command.params;
+  async function bookByFloor(command) {
+    //book_by_floor 1 TonyStark 48
+    const [floor, name, age] = command.params;
 
-  try {
-    const guest = new Guest(name, age);
-    const roomsOnFloor = await services.bookByFloor(floor, guest);
-    // [{keycardNumber, roomNumber}, {keycardNumber, roomNumber}]
-    const rooms = roomsOnFloor.map((room) => room.roomNumber);
-    const keycardNumbers = roomsOnFloor.map((room) => room.keycardNumber);
+    try {
+      const guest = new Guest(name, age);
+      const roomsOnFloor = await services.bookByFloor(floor, guest);
+      // [{keycardNumber, roomNumber}, {keycardNumber, roomNumber}]
+      const rooms = roomsOnFloor.map((room) => room.roomNumber);
+      const keycardNumbers = roomsOnFloor.map((room) => room.keycardNumber);
+      console.log(
+        `Room ${rooms.join(
+          ", "
+        )} are booked with keycard number ${keycardNumbers.join(", ")}`
+      );
+    } catch (err) {
+      switch (true) {
+        case err instanceof RoomFloorIsAlreadyBookedError: {
+          console.log(`Cannot book floor ${floor} for ${name}.`);
+          break;
+        }
+        default:
+          throw err;
+      }
+    }
+  }
+
+  async function checkout(command) {
+    //checkout 4 TonyStark
+    const [keycardNumber, name] = command.params;
+
+    try {
+      const room = await services.checkout(keycardNumber, name);
+      console.log(`Room ${room.roomNumber} is checkout.`);
+    } catch (err) {
+      switch (true) {
+        case err instanceof GuestNotMatchKeycardNumberError: {
+          console.log(
+            `Only ${err.room.guest.name} can checkout with keycard number ${keycardNumber}.`
+          );
+          break;
+        }
+        case err instanceof CheckoutAvailableRoomError: {
+          console.log("You can not checkout available room");
+          break;
+        }
+        default:
+          throw err;
+      }
+    }
+  }
+
+  async function checkoutGuestByFloor(command) {
+    //checkout_guest_by_floor 1
+    const [floor] = command.params;
+
+    try {
+      const roomsOnFloor = await services.checkoutGuestByFloor(floor);
+      console.log(
+        `Room ${roomsOnFloor
+          .map((room) => room.roomNumber)
+          .join(", ")} are checkout.`
+      );
+    } catch (err) {
+      switch (true) {
+        case err instanceof CheckoutAvailableRoomFloorError: {
+          console.log(`No room on floor ${floor} was booked.`);
+          break;
+        }
+        default:
+          throw err;
+      }
+    }
+  }
+
+  async function listAvailableRooms(command) {
     console.log(
-      `Room ${rooms.join(
-        ", "
-      )} are booked with keycard number ${keycardNumbers.join(", ")}`
+      (await services.listAvailableRooms())
+        .map((availableRoom) => availableRoom.roomNumber)
+        .join(", ")
     );
-  } catch (err) {
-    switch (true) {
-      case err instanceof RoomFloorIsAlreadyBookedError: {
-        console.log(`Cannot book floor ${floor} for ${name}.`);
-        break;
-      }
-      default:
-        throw err;
-    }
   }
-}
 
-async function checkout(command) {
-  //checkout 4 TonyStark
-  const [keycardNumber, name] = command.params;
+  async function listGuests(command) {
+    //list_guest
+    const allGuests = (await services.listGuests()).map((guest) => guest.name);
 
-  try {
-    const room = await services.checkout(keycardNumber, name);
-    console.log(`Room ${room.roomNumber} is checkout.`);
-  } catch (err) {
-    switch (true) {
-      case err instanceof GuestNotMatchKeycardNumberError: {
-        console.log(
-          `Only ${err.room.guest.name} can checkout with keycard number ${keycardNumber}.`
-        );
-        break;
-      }
-      case err instanceof CheckoutAvailableRoomError: {
-        console.log("You can not checkout available room");
-        break;
-      }
-      default:
-        throw err;
-    }
+    console.log(allGuests.join(", "));
   }
-}
 
-async function checkoutGuestByFloor(command) {
-  //checkout_guest_by_floor 1
-  const [floor] = command.params;
+  async function listGuestsByAge(command) {
+    //list_guest_by_age < 18
+    const [operation, age] = command.params;
 
-  try {
-    const roomsOnFloor = await services.checkoutGuestByFloor(floor);
+    const guestsByAge = await services.listGuestsNameByAge(operation, age);
+
+    console.log(guestsByAge.join(", "));
+  }
+
+  async function listGuestsByFloor(command) {
+    //list_guest_by_floor 2
+    const [floor] = command.params;
+
+    const guestsByFloor = await services.listGuestsNameByFloor(floor);
+
+    console.log(guestsByFloor.join(", "));
+  }
+
+  async function getGuestsInRoom(command) {
+    //get_guest_in_room 203
+    const [roomNumber] = command.params;
+
     console.log(
-      `Room ${roomsOnFloor
-        .map((room) => room.roomNumber)
-        .join(", ")} are checkout.`
+      (await services.getRoomByRoomNumber(roomNumber.toString())).guest.name
     );
-  } catch (err) {
-    switch (true) {
-      case err instanceof CheckoutAvailableRoomFloorError: {
-        console.log(`No room on floor ${floor} was booked.`);
-        break;
-      }
-      default:
-        throw err;
-    }
   }
-}
 
-async function listAvailableRooms(command) {
-  console.log(
-    (await services
-      .listAvailableRooms())
-      .map((availableRoom) => availableRoom.roomNumber)
-      .join(", ")
-  );
-}
-
-async function listGuests(command) {
-  //list_guest
-  const allGuests = (await services.listGuests()).map((guest) => guest.name);
-
-  console.log(allGuests.join(", "));
-}
-
-async function listGuestsByAge(command) {
-  //list_guest_by_age < 18
-  const [operation, age] = command.params;
-
-  const guestsByAge = await services
-    .listGuestsNameByAge(operation, age);
-
-  console.log(guestsByAge.join(", "));
-}
-
-async function listGuestsByFloor(command) {
-  //list_guest_by_floor 2
-  const [floor] = command.params;
-
-  const guestsByFloor = await services.listGuestsNameByFloor(floor);
-
-  console.log(guestsByFloor.join(", "));
-}
-
-async function getGuestsInRoom(command) {
-  //get_guest_in_room 203
-  const [roomNumber] = command.params;
-
-  console.log((await services.getRoomByRoomNumber(roomNumber.toString())).guest.name);
+  return {
+    createHotel,
+    book,
+    bookByFloor,
+    checkout,
+    checkoutGuestByFloor,
+    listAvailableRooms,
+    listGuests,
+    listGuestsByAge,
+    listGuestsByFloor,
+    getGuestsInRoom,
+  };
 }
 
 main();
--- services.js
+++ services.js
@@ -7,151 +7,156 @@ const {
   CheckoutAvailableRoomError,
 } = require("./error");
 
-const {
-  createKeycards,
-  generateKeycard,
-  getRoomByRoomNumber,
-  listAvailableRooms,
-  createRooms,
-  listBookedRoom,
-  getRoomByKeycardNumber,
-  returnKeycard,
-  listRooms,
-  saveRoom
-} = require("./firestore-repositories");
 
-async function isHotelFullyBooked() {
-  return (await listBookedRoom()).length === (await listRooms()).length;
-}
+function createService(repository) {
+  const {
+    createKeycards,
+    generateKeycard,
+    getRoomByRoomNumber,
+    listAvailableRooms,
+    createRooms,
+    listBookedRoom,
+    getRoomByKeycardNumber,
+    returnKeycard,
+    listRooms,
+    saveRoom
+  } = repository;
 
-async function hasBookedRoomOnFloor(floor) {
-  return (await listBookedRoomsByFloor(floor)).length > 0;
-}
-
-async function listBookedRoomsByFloor(floor) {
-  return (await listBookedRoom()).filter((room) => room.floor === floor);
-}
-
-async function checkoutRoomByFloor(roomsOnFloor) {
-  await roomsOnFloor.reduce(async (initial, room) => {
-    await initial;
-    await checkout(
-      room.keycardNumber,
-      room.guest.name
-    )
-
-    return Promise.resolve()
-  }, Promise.resolve())
-
-}
-
-async function listRoomsByFloor(floor) {
-  return (await listAvailableRooms()).filter((room) => room.floor === floor);
-}
-
-async function createHotel(floor, roomPerFloor) {
-  const hotel = { floor, roomPerFloor };
-
-  if (roomPerFloor > 99)
-    throw new Error("Can not create hotel with 100 or more rooms per floor");
-
-  // await firestoreRepo.createKeycards(floor, roomPerFloor);
-  await createKeycards(floor, roomPerFloor);
-  await createRooms(floor, roomPerFloor);
-
-  return hotel;
-}
-
-async function book(roomNumber, guest) {
-  if (await isHotelFullyBooked()) {
-    throw new HotelIsFullError();
+  async function isHotelFullyBooked() {
+    return (await listBookedRoom()).length === (await listRooms()).length;
+  }
+  
+  async function hasBookedRoomOnFloor(floor) {
+    return (await listBookedRoomsByFloor(floor)).length > 0;
+  }
+  
+  async function listBookedRoomsByFloor(floor) {
+    return (await listBookedRoom()).filter((room) => room.floor === floor);
+  }
+  
+  async function checkoutRoomByFloor(roomsOnFloor) {
+    await roomsOnFloor.reduce(async (initial, room) => {
+      await initial;
+      await checkout(
+        room.keycardNumber,
+        room.guest.name
+      )
+  
+      return Promise.resolve()
+    }, Promise.resolve())
+  
+  }
+  
+  async function listRoomsByFloor(floor) {
+    return (await listAvailableRooms()).filter((room) => room.floor === floor);
+  }
+  
+  async function createHotel(floor, roomPerFloor) {
+    const hotel = { floor, roomPerFloor };
+  
+    if (roomPerFloor > 99)
+      throw new Error("Can not create hotel with 100 or more rooms per floor");
+  
+    // await firestoreRepo.createKeycards(floor, roomPerFloor);
+    await createKeycards(floor, roomPerFloor);
+    await createRooms(floor, roomPerFloor);
+  
+    return hotel;
+  }
+  
+  async function book(roomNumber, guest) {
+    if (await isHotelFullyBooked()) {
+      throw new HotelIsFullError();
+    }
+  
+    const room = await getRoomByRoomNumber(roomNumber);
+    if (!room.isAvailable) {
+      throw new RoomIsAlreadyBookedError(room);
+    }
+  
+    const keycardNumber = await generateKeycard();
+    const updatedBook = room.book(guest, keycardNumber);
+    await saveRoom(updatedBook)
+    return keycardNumber;
+  }
+  
+  async function bookByFloor(floor, guest) {
+    if (await hasBookedRoomOnFloor(floor)) {
+      throw new RoomFloorIsAlreadyBookedError(floor, guest.name);
+    }
+    const roomsOnFloor = await listRoomsByFloor(floor);
+  
+    await roomsOnFloor.reduce(async (initial, room) => {
+      await initial;
+      await book(room.roomNumber, guest)
+  
+      return Promise.resolve()
+    }, Promise.resolve())
+  
+    return listBookedRoomsByFloor(floor)
+  }
+  
+  async function checkout(keycardNumber, name) {
+    const room = await getRoomByKeycardNumber(keycardNumber);
+    if (!room) {
+      throw new CheckoutAvailableRoomError();
+    }
+    //there is the room that booked with this keycard number
+    if (room.guest.name != name) {
+      throw new GuestNotMatchKeycardNumberError(room);
+    }
+    //name match with keycardNumber
+    //checkout
+    await returnKeycard(room);
+    const updatedRoom = room.checkout();
+    await saveRoom(updatedRoom);
+    return updatedRoom;
+  }
+  
+  async function checkoutGuestByFloor(floor) {
+    if (!await hasBookedRoomOnFloor(floor)) {
+      throw new CheckoutAvailableRoomFloorError();
+    }
+    //there is some room on that floor was booked
+    //checkout that room
+    const roomsOnFloor = await listBookedRoomsByFloor(floor);
+  
+    await checkoutRoomByFloor(roomsOnFloor);
+    return roomsOnFloor;
+  }
+  
+  async function listGuests() {
+    return result = (await listBookedRoom()).map((room) => room.guest);
+  }
+  
+  async function listGuestsNameByFloor(floor) {
+    return Array.from(
+      new Set((await listBookedRoomsByFloor(floor)).map((room) => room.guest.name))
+    );
+  }
+  
+  async function listGuestsNameByAge(operation, age) {
+    return Array.from(
+      new Set(
+        (await listGuests())
+          .filter((guest) => eval(guest.age + operation + age))
+          .map((guest) => guest.name)
+      )
+    );
   }
 
-  const room = await getRoomByRoomNumber(roomNumber);
-  if (!room.isAvailable) {
-    throw new RoomIsAlreadyBookedError(room);
-  }
-
-  const keycardNumber = await generateKeycard();
-  const updatedBook = room.book(guest, keycardNumber);
-  await saveRoom(updatedBook)
-  return keycardNumber;
+  return {
+    createHotel,
+    book,
+    getRoomByRoomNumber,
+    bookByFloor,
+    checkoutGuestByFloor,
+    listAvailableRooms,
+    checkout,
+    listGuests,
+    listGuestsNameByFloor,
+    listGuestsNameByAge,
+  };
 }
 
-async function bookByFloor(floor, guest) {
-  if (await hasBookedRoomOnFloor(floor)) {
-    throw new RoomFloorIsAlreadyBookedError(floor, guest.name);
-  }
-  const roomsOnFloor = await listRoomsByFloor(floor);
-
-  await roomsOnFloor.reduce(async (initial, room) => {
-    await initial;
-    await book(room.roomNumber, guest)
-
-    return Promise.resolve()
-  }, Promise.resolve())
-
-  return listBookedRoomsByFloor(floor)
-}
-
-async function checkout(keycardNumber, name) {
-  const room = await getRoomByKeycardNumber(keycardNumber);
-  if (!room) {
-    throw new CheckoutAvailableRoomError();
-  }
-  //there is the room that booked with this keycard number
-  if (room.guest.name != name) {
-    throw new GuestNotMatchKeycardNumberError(room);
-  }
-  //name match with keycardNumber
-  //checkout
-  await returnKeycard(room);
-  const updatedRoom = room.checkout();
-  await saveRoom(updatedRoom);
-  return updatedRoom;
-}
-
-async function checkoutGuestByFloor(floor) {
-  if (!await hasBookedRoomOnFloor(floor)) {
-    throw new CheckoutAvailableRoomFloorError();
-  }
-  //there is some room on that floor was booked
-  //checkout that room
-  const roomsOnFloor = await listBookedRoomsByFloor(floor);
-
-  await checkoutRoomByFloor(roomsOnFloor);
-  return roomsOnFloor;
-}
-
-async function listGuests() {
-  return result = (await listBookedRoom()).map((room) => room.guest);
-}
-
-async function listGuestsNameByFloor(floor) {
-  return Array.from(
-    new Set((await listBookedRoomsByFloor(floor)).map((room) => room.guest.name))
-  );
-}
-
-async function listGuestsNameByAge(operation, age) {
-  return Array.from(
-    new Set(
-      (await listGuests())
-        .filter((guest) => eval(guest.age + operation + age))
-        .map((guest) => guest.name)
-    )
-  );
-}
-
-module.exports = {
-  createHotel,
-  book,
-  getRoomByRoomNumber,
-  bookByFloor,
-  checkoutGuestByFloor,
-  listAvailableRooms,
-  checkout,
-  listGuests,
-  listGuestsNameByFloor,
-  listGuestsNameByAge,
-};
+module.exports = createService;
